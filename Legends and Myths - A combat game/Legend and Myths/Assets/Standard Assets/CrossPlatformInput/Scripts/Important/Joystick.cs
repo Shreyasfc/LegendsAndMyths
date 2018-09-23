d8758564aa77f3d21dfad98949e7cdd1642e2bdd
Shreyas Parida <41685241+Shreyasfc@users.noreply.github.com>
2018-09-23 16:46:44 +0200

    Update a bit

--- Legends and Myths - A combat game/Legend and Myths/Assets/Standard Assets/CrossPlatformInput/Scripts/Important/Joystick.cs
+++ Legends and Myths - A combat game/Legend and Myths/Assets/Standard Assets/CrossPlatformInput/Scripts/Important/Joystick.cs
@@ -8,22 +8,22 @@ namespace UnityStandardAssets.CrossPlatformInput
 	{
 		public enum AxisOption
 		{
-			// Options for which axes to use
-			Both, // Use both
+			
+			Both,
 			OnlyHorizontal, // Only horizontal
 			OnlyVertical // Only vertical
 		}
 
 		public int MovementRange = 100;
-		public AxisOption axesToUse = AxisOption.Both; // The options for the axes that the still will use
-		public string horizontalAxisName = "Horizontal"; // The name given to the horizontal axis for the cross platform input
-		public string verticalAxisName = "Vertical"; // The name given to the vertical axis for the cross platform input
+		public AxisOption axesToUse = AxisOption.Both; 
+		public string horizontalAxisName = "Horizontal"; 
+		public string verticalAxisName = "Vertical";
 
 		Vector3 m_StartPos;
 		bool m_UseX; // Toggle for using the x axis
 		bool m_UseY; // Toggle for using the Y axis
-		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
-		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // Reference to the joystick in the cross platform input
+		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // The joystick in the cross platform input
+		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // The joystick in the cross platform input
 
 		void OnEnable()
 		{
